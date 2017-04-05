72c6f97bc110142737bd02280589afa69ee997c7
Ravil Garipov <ravildgar@gmail.com>
2017-04-05 16:07:38 +0000

    use default props values

--- public/app.jsx
+++ public/app.jsx
@@ -1,15 +1,26 @@
 var Greeter = React.createClass({
+    getDefaultProps: function (){
+        return {
+          name: 'Sveta',
+          message: 'This is from a component! Default state.'
+        };
+    },
     render: function() {
+        var name = this.props.name;
+        var message = this.props.message;
         return (
             <div>
-                <h1>Hello React!</h1>
-                <p>This is from a component!</p>
+                <h1>Hello {name}!</h1>
+                <p>{message}</p>
             </div>
         );
     }
 });
 
+var firstName = 'Ravil';
+var message = 'This is from a component!';
+
 ReactDOM.render(
-    <Greeter />,
+    <Greeter name={firstName} message={message}/>,
     document.getElementById('app')
 );

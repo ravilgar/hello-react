3b3fe2cb24cb2f7692f4ddc5b47f0f50247b6c3d
Ravil Garipov <ravildgar@gmail.com>
2017-04-12 19:50:34 +0000

    add nested component

--- public/app.jsx
+++ public/app.jsx
@@ -1,17 +1,55 @@
+var GreeterMessage = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <h1>Some h1</h1>
+                <p>some paragraph </p>
+            </div>
+        );
+    }
+});
+
 var Greeter = React.createClass({
-    getDefaultProps: function (){
+    getDefaultProps: function() {
         return {
-          name: 'Sveta',
-          message: 'This is from a component! Default state.'
+            name: 'React',
+            message: 'This is from a component! Default state.'
         };
     },
+    getInitialState: function() {
+        return {
+            name: this.props.name
+        };
+    },
+    onButtonClick: function(e) {
+        e.preventDefault();
+
+        var nameRef = this.refs.name;
+
+        var name = nameRef.value;
+        nameRef.value = '';
+
+        if (typeof name === 'string' && name.length > 0) {
+            this.setState({
+                name: name
+            });
+        }
+    },
     render: function() {
-        var name = this.props.name;
+        var name = this.state.name;
         var message = this.props.message;
         return (
             <div>
                 <h1>Hello {name}!</h1>
                 <p>{message}</p>
+                
+                <GreeterMessage/>
+                <form onSubmit={this.onButtonClick}>
+                    <input type="text" ref="name"/>
+                    <button>
+                        Set name
+                    </button>
+                </form>
             </div>
         );
     }
